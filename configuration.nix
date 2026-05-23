6bca11ddce459101f368be4aeb7650c59a523406
sea vuh <sea@vuh.com>
2026-05-23 09:10:58 +0530

    native scaling and remove core apps

--- configuration.nix
+++ configuration.nix
@@ -89,6 +89,27 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
+  # Disable GNOME application suite
+  services.gnome.core-apps.enable = false;
+  services.gnome.core-developer-tools.enable = false;
+  services.gnome.games.enable = false;
+  environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
+
+  programs.dconf.profiles.user.databases = [
+    {
+      settings = {
+        "org/gnome/mutter" = {
+          experimental-features = [
+            "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+            "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+            "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+            "autoclose-xwayland" # automatically terminates Xwayland if all relevant X11 clients are gone      
+          ];
+        };
+      };
+    }
+  ];
+
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
